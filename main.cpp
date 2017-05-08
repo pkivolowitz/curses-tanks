8ae7c82f42492ba05d150612280a2d052fcc2f61
ErynBenner <ebenner@carthage.edu>
2017-05-07 21:09:46 -0500

    Updated Main Menu with Grafix

--- main.cpp
+++ main.cpp
@@ -42,19 +42,147 @@ void DrawScreen(Ground & g, Player * players, int turn)
 	players[1].DrawSettings(turn);
 	refresh();
 }
-void MainMenu()
+int MainMenu()
+
 {
-	start_color();
-	init_pair(1, COLOR_RED, COLOR_BLACK);
-	attron(COLOR_PAIR(1));
-	move(1,0);
+	int rv = 0;
+	//init_pair(1, COLOR_RED, COLOR_BLACK);
+
+	//attron(COLOR_PAIR(1));
+
+	WINDOW *w;
+	w = newwin(10, 12, 1, 1);
+	box(w, 0, 0);
 	stringstream ss;
+
+	//Welcome to Tanks! Sign//
+	ss << " _    _      _                            _          _____           _        _";
+	mvaddstr(1, COLS /2 - 42, ss.str().c_str());
+
 	ss = stringstream();
-	ss << "Welcome to Tanks!";
+	ss << "| |  | |    | |                          | |        |_   _|         | |      | |";
+	mvaddstr(2, COLS / 2 - 42, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___     | | __ _ _ __ | | _____| |";
+	mvaddstr(3, COLS / 2 - 42, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| |/\\| |/ _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\    | |/ _` | '_ \\| |/ / __| |";
+	mvaddstr(4, COLS / 2 - 42, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "\\  /\\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |   | | (_| | | | |   <\\__ \\_|";
+	mvaddstr(5, COLS / 2 - 42, ss.str().c_str());
+
+	ss = stringstream();
+	ss << " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/    \\_/\\__,_|_| |_|_|\\_\\___(_)";
+	mvaddstr(6, COLS / 2 - 42, ss.str().c_str());
+
+	//Created by Sign//
 	
+	ss = stringstream();
+	ss << "  ___              _          _   _         ";
+	mvaddstr(8, COLS / 2 - 24,ss.str().c_str());
+
+	ss = stringstream();
+	ss << " / __|_ _ ___ __ _| |_ ___ __| | | |__ _  _ ";
+	mvaddstr(9, COLS / 2 - 24, ss.str().c_str());
+	
+	ss = stringstream();
+	ss << "| (__| '_/ -_) _` |  _/ -_) _` | | '_ \\ || |";
+	mvaddstr(10, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	ss << " \\___|_| \\___\\__,_|\\__\\___\\__,_| |_.__/\\_, |";
+	mvaddstr(11, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "                                       |__/ ";
+	mvaddstr(12, COLS / 2 - 24, ss.str().c_str());
+
+	//Nick Ressler Sign//
+
+	ss = stringstream();
+	ss << " _  _ _    _     ___           _         ";
+	mvaddstr(13, COLS / 2 - 23, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| \\| (_)__| |__ | _ \\___ _____| |___ _ _ ";
+	mvaddstr(14, COLS / 2 - 23, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| .` | / _| / / |   / -_|_-<_-< / -_) '_|";
+	mvaddstr(15, COLS / 2 - 23, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "|_|\\_|_\\__|_\\_\\ |_|_\\___/__/__/_\\___|_|  ";
+	mvaddstr(16, COLS / 2 - 23, ss.str().c_str());
+
+	//& Sign//
+
+	ss = stringstream();
+	ss << " __     ";
+	mvaddstr(17, COLS / 2 - 4, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "/ _|___ ";
+	mvaddstr(18, COLS / 2 - 4, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "> _|_ _|";
+	mvaddstr(19, COLS / 2 - 4, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "\\_____| ";
+	mvaddstr(20, COLS / 2 - 4, ss.str().c_str());
+
+	//Eryn Benner Sign//
+
+	ss = stringstream();
+	ss << " ___                 ___                        ";
+	mvaddstr(21, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| __|_ _ _  _ _ _   | _ ) ___ _ _  _ _  ___ _ _ ";
+	mvaddstr(22, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "| _|| '_| || | ' \\  | _ \\/ -_) ' \\| ' \\/ -_) '_|";
+	mvaddstr(23, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	ss << "|___|_|  \\_, |_||_| |___/\\___|_||_|_||_\\___|_|  ";
+	mvaddstr(24, COLS / 2 - 24, ss.str().c_str());
+
+	ss = stringstream();
+	move(LINES - 2, 43);
+	ss << "Press The 'E' Key to Continue";
 	addstr(ss.str().c_str());
 
+	char c = getch();
+	switch (c)
+
+	{
+
+	case 'e':
+
+	case 'E':
+
+		rv = 4;
+
+		break;
+
+	}
+
+	//add a case where if none of the options are chosen nothing happens
+
+	noecho();
+	return rv;
+
 }
+
+bool hit = false;
 /*
 int HitBox(int P1 , int shotl, int shotc, int turn)
 {
@@ -78,10 +206,8 @@ class Vec2D
 {
 
 };
-void Shoot(Ground & g, Player * players, int turn, int bulleth, int bulletv)
+void Shoot(Ground & g, Player * players, int turn, double bulleth, double bulletv, bool hit)
 {
-
-
 	double angle = players[turn].angle / 180.0 * PI;
 	//vertical
 	double y_component = sin(angle) * players[turn].power * 0.2;
@@ -100,7 +226,6 @@ void Shoot(Ground & g, Player * players, int turn, int bulleth, int bulletv)
 	// higher ground numbers are lower altitudes (0 is first line, etc).
 
 	p0y = LINES - p0y;
-
 
 	for (int i = 1; i < 5000; i++)
 	{
@@ -121,56 +246,115 @@ void Shoot(Ground & g, Player * players, int turn, int bulleth, int bulletv)
 		//if bullet goes off the screen at the bottom
 		if (pNy >= LINES - 2)
 			break;
+		if (pNy >= LINES - 2)
 
+			break;
+
+
+
+		//Bullet wont go through the ground
+
+		if (pNy >= g.ground.at((int)pNx))
+
+		{
+			g.ground.at((int)pNx)++;
+
+			g.ground.at((int)pNx - 1)++;
+
+			g.ground.at((int)pNx + 1)++;
+
+			break;
+
+		}
 		//this makes the bullet only one
 		erase();
 		DrawScreen(g, players, turn);
-		move((int)pNy - 1, (int)pNx + 1);
-		addch(ACS_LANTERN);
+		mvaddch((int)pNy - 1, (int)pNx + 1, ACS_LANTERN);
 
 		refresh();
-		Sleep(100);
-	}
-	//if bomb is within 1 column in either direction of player 1 or on the column
-	if (bulleth == players[0].col || bulleth == players[0].col + 1 || bulleth == players[0].col - 1)
-	{
-		if (bulletv == players[0].line || bulletv == players[0].line + 1 || bulletv == players[0].line - 1)
+
+		// super fast
+		if (players[turn].power >= 90)
 		{
+			Sleep(50);
+		}
+
+		//fast
+		if (players[turn].power > 75 && players[turn].power < 90)
+
+		{
+			Sleep(90);
+		}
+
+		//average
+		if (players[turn].power > 50 && players[turn].power < 75)
+
+		{
+			Sleep(125);
+		}
+
+		//slower
+		if (players[turn].power > 25 && players[turn].power < 50)
+
+		{
+			Sleep(150);
+		}
+
+		//super slow
+		if (players[turn].power <= 25)
+
+		{
+			Sleep(175);
+		}
+
+		bulleth = pNx; //y
+		bulletv = pNy; //x
+
+		//if bomb is within 1 column in either direction of player 1 or on the column
+		//                         line - 1                           
+		//                             ^
+		//             |           ____|__
+		// col - 1 <-- |                  |
+		//		       |_______           | --> col + 1
+		//                 |              |
+		//                 V
+		//               line + 1
+		if (bulletv >= players[0].col - 1 && bulleth <= players[0].line + 1 || bulletv <= players[0].col + 1 && bulleth >= players[0].col - 1)
+		{
+			hit = true;
+			break;
+		}
+		if (hit == true)
 			players[0].health--;
-		}
-	}
-	//if bomb is within 1 column in either direction of player 2
-	if (bulleth == players[1].col || bulleth == players[1].col + 1 || bulleth == players[1].col - 1)
-	{
-		if (bulletv == players[1].line || bulletv == players[1].line + 1 || bulletv == players[1].line - 1)
+		if (bulletv >= players[1].col - 1 && bulleth <= players[1].line + 1 || bulletv <= players[1].col + 1 && bulleth >= players[1].col - 1)
 		{
-			players[1].health--;
+			hit = true;
+			break;
 		}
+		if (hit == true)
+			players[1].health--;
+		/*Testing bullet coordinates*/
+		/*stringstream ss;
+		ss = stringstream();
+		ss << "col: " << bulleth;
 
-}
-	bulleth = pNx + 1;
-	bulletv = pNy - 1;
+		mvaddstr(1, COLS / 2 - 3, ss.str().c_str());
+		refresh();
 
-	stringstream ss;
-	ss = stringstream();
-	ss << "col: " << bulleth;
-	move(1, COLS / 2 - 3);
-	addstr(ss.str().c_str());
-	refresh();
+		ss = stringstream();
+		ss << "line: " << bulletv;
 
-	ss = stringstream();
-	ss << "line: " << bulletv;
-	move(2, COLS / 2 - 3);
-	addstr(ss.str().c_str());
-	refresh();
+		mvaddstr(2, COLS / 2 - 3, ss.str().c_str());
+		refresh();
 
-	ss = stringstream();
-	ss << "#";
-	move(bulletv, bulleth);
-	addstr(ss.str().c_str());
-	refresh();
+		ss = stringstream();
+		ss << "#";
+		mvaddstr(bulletv, bulleth, ss.str().c_str());
+		refresh();
 
-	Sleep(1200);
+		Sleep(1200);*/
+	}
+	
 }
 int main(int argc, char * argv[])
 {
@@ -182,12 +366,24 @@ int main(int argc, char * argv[])
 	Player players[2];
 	
 	initscr();
-	noecho();
-	while (!getch() == KEY_ENTER)
+	while (true)
+
 	{
-		MainMenu();
-		
+		bool quit;
+		quit = true;
+		keypad(stdscr, 1);
+		int x = 0;
+		x = MainMenu();
+
+		//quit
+		if (x == 4)
+
+		{
+			quit = false;
+			break;
+		}
 	}
+
 	clear();
 	keypad(stdscr, 1);
 	int bulleth = 0;
@@ -209,11 +405,11 @@ int main(int argc, char * argv[])
 			keep_going = false;
 			break;
 
-		case '<':
+		case KEY_DOWN:
 			players[turn].PowerDown();
 			break;
 
-		case '>':
+		case KEY_UP:
 			players[turn].PowerUp();
 			break;
 
@@ -221,7 +417,7 @@ int main(int argc, char * argv[])
 			players[turn].AngleUp();
 			break;
 
-		case 'd':
+		case KEY_LEFT:
 			players[turn].AngleDown();
 			break;
 
@@ -230,7 +426,7 @@ int main(int argc, char * argv[])
 #if defined(WIN32)
 		case PADENTER:
 #endif
-			Shoot(g, players, turn, bulleth, bulletv);
+			Shoot(g, players, turn, bulleth, bulletv, hit);
 			//HitBox(players[1].Initialize(), )
 			turn = 1 - turn;
 			break;
@@ -241,10 +437,9 @@ int main(int argc, char * argv[])
 		}
 		DrawScreen(g, players, turn);
 		if (show_char) {
-			move(0, 1);
 			stringstream ss;
 			ss << setw(4) << c << " ";
-			addstr(ss.str().c_str());
+			mvaddstr(0, 1, ss.str().c_str());
 			refresh();
 		}
 	}
